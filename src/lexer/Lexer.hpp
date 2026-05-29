4df564a24c8b14064c3b11726df7b0fe02839773
Reyesalv20 <ruthreyes20@unitec.edu>
2026-05-29 14:47:58 -0600

    Lexer.hpp con TokenId, struct Token y clase Lexer

---
+++ src/lexer/Lexer.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <string>
+#include <istream>
+
+enum class TokenId{
+INT_LIT,
+STRING_LIT,
+KW_VAR,
+KW_FUNC,
+KW_RETURN,
+KW_IF,
+KW_ELSE,
+KW_FOR,
+KW_BOOL,
+KW_TRUE,
+KW_FALSE,
+KW_INT,
+KW_REF,
+KW_PRINT,
+KW_PRINTLN,
+IDENT,
+OP_ADD,
+OP_SUB,
+OP_MUL,
+OP_DIV,
+OP_MOD,
+OP_EQ,
+OP_NEQ,
+OP_LT,
+OP_GT,
+OP_LTE,
+OP_GTE,
+OP_AND,
+OP_OR,
+OP_NOT,
+OP_ASSIGN,
+OP_SHORT_DECL,
+OP_ADDR,
+OPEN_PAR,
+CLOSE_PAREN,
+OPEN_BRACE,
+CLOSE_BRACE,
+COMMA,
+SEMICOLON,
+END_OF_FILE,
+UNK
+};
+
+struct Token{
+    TokenId tokenId;
+    std::string text;
+    int line;
+    int column;
+};
+
+class Lexer{
+public:
+    explicit Lexer(std::istream& input):input_(input){consume();}
+    Token nextToken();
+private:
+void consume(){
+    ch=input_.get();
+}
+std::istream& input_;
+char ch;
+
+};
